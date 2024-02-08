938d9d0849ca599223470c4488874d19b1f4341c
Aayush-G18 <goyalaayush292@gmail.com>
2024-02-09 00:39:19 +0530

    1.Randomized color for each square 2.grid size and location fixed 3.prompt for grid size done

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const body = document.querySelector("body");
 const container = body.querySelector(".container");
-
 let newdiv = document.createElement("div");
 newdiv.style.border = "1px solid black";
 newdiv.classList.add("child");
+const button=body.querySelector("button");
 
-for (let i = 1; i <= 16; i++) {
-    container.appendChild(newdiv.cloneNode(true)); // Append a clone of newdiv
-}
+button.addEventListener('click',function(){
+    let size=prompt("Enter grid size:");
+    container.innerHTML='';
+    for (let i = 1; i <= size**2; i++) {
+        let divToAdd=newdiv.cloneNode(true);
+        container.appendChild(divToAdd); 
+        divToAdd.style.flexBasis=`${100/size}%`
+        divToAdd.addEventListener('mouseover',function() {
+            divToAdd.style.backgroundColor=`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;
+        })
+    }
+})
